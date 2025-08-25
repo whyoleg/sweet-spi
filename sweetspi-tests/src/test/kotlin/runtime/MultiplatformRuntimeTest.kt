12c21e88722243a89978e517b3e907fc5066ac0e
Oleg Yukhnevich <whyoleg@gmail.com>
2025-08-25 21:16:05 +0300

    Add test with explicit services in `ServiceProvider`

--- sweetspi-tests/src/test/kotlin/runtime/MultiplatformRuntimeTest.kt
+++ sweetspi-tests/src/test/kotlin/runtime/MultiplatformRuntimeTest.kt
@@ -76,4 +76,79 @@ class MultiplatformRuntimeTest : AbstractTest() {
             assert(tasks.any { it.path in nativeTestTasks && it.outcome.isPositive })
         }
     }
+
+    @FastVersionedTest
+    fun testAllTargetsExplicitServiceProvider(versions: TestVersions) {
+        val project = project(versions) {
+
+            withSweetSpi()
+            allTargets()
+            kotlinTest(COMMON_TEST)
+            prepend(BUILD_GRADLE_KTS) {
+                // for native tasks
+                "import org.jetbrains.kotlin.gradle.plugin.mpp.*"
+            }
+            append(BUILD_GRADLE_KTS) {
+                """
+                kotlin {
+                  // setup tests running in RELEASE mode
+                  targets.withType<KotlinNativeTarget>().configureEach {
+                      binaries.test(listOf(NativeBuildType.RELEASE))
+                  }
+                  targets.withType<KotlinNativeTargetWithTests<*>>().configureEach {
+                      testRuns.create("releaseTest") {
+                          setExecutionSourceFrom(binaries.getTest(NativeBuildType.RELEASE))
+                      }
+                  }
+                }
+                """.trimIndent()
+            }
+            kotlinSourceFile(
+                sourceSet = COMMON_MAIN, path = "main.kt",
+                code = """
+                @Service interface SimpleService1
+                @Service interface SimpleService2
+                @ServiceProvider(SimpleService1::class) object SimpleServiceImpl : SimpleService1, SimpleService2
+                """.trimIndent()
+            )
+            kotlinSourceFile(
+                sourceSet = COMMON_TEST, path = "test.kt",
+                code = """
+                import kotlin.test.*
+                
+                class SimpleTest {
+                    @Test
+                    fun doTestService1() {
+                        val services = ServiceLoader.load<SimpleService1>()
+                        assertEquals(1, services.size)
+                        val service = services.single()
+                        assertEquals(SimpleServiceImpl, service)
+                    }
+
+                    @Test
+                    fun doTestService2() {
+                        val services = ServiceLoader.load<SimpleService2>()
+                        assertEquals(0, services.size)
+                    }
+                }
+                """.trimIndent()
+            )
+        }
+        project.gradle("build") {
+            assert(task(":jvmTest")!!.outcome.isPositive)
+            assert(task(":jsTest")!!.outcome.isPositive)
+            assert(task(":wasmJsTest")!!.outcome.isPositive)
+            assert(task(":wasmWasiTest")!!.outcome.isPositive)
+
+            // tasks are different on different OS, only desktop targets are mentioned
+            val nativeTestTasks = setOf(
+                ":macosArm64Test",
+                ":macosX64Test",
+                ":linuxX64Test",
+                ":linuxArm64Test",
+                ":mingwX64Test",
+            )
+            assert(tasks.any { it.path in nativeTestTasks && it.outcome.isPositive })
+        }
+    }
 }
