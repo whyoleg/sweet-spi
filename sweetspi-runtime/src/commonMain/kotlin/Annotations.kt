171ce9635b8310403ac83b367b2395926b34a961
Oleg Yukhnevich <whyoleg@gmail.com>
2025-11-05 19:34:52 +0200

    ideas

--- sweetspi-runtime/src/commonMain/kotlin/Annotations.kt
+++ sweetspi-runtime/src/commonMain/kotlin/Annotations.kt
@@ -9,7 +9,11 @@ import kotlin.reflect.*
 @MustBeDocumented
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.BINARY)
-public annotation class Pluggable
+public annotation class Pluggable {
+    public interface Provider<T : Any> {
+        public fun provide(): T
+    }
+}
 
 @Repeatable
 @MustBeDocumented
@@ -24,12 +28,59 @@ public annotation class Plugin(public val pluggable: KClass<*>)
 public annotation class PluginInfo
 
 /**
+ * In case of JVM, we do have information about KClass (and annotation) of the `Plugin`
+ * In case of Wasm/Js/etc, we COULD have an some metadata stored somewhere, before we will try to load implementation,
+ *   but the loading will happen in an `async` way, and so it will probably be `interface FleetPlugin { suspend fun loadPlugin() }` and so
+ *   this `FleetPlugin` instances can have metadata
+ *   It feels like having `PluginInfo` is really not useful...
+ *
+ */
+
+// from where could plugins in the case of Wasm/JS/browser come?
+
+@Pluggable
+public interface LoggingBackend {
+    // generated to support functions/properties/objects on JVM
+    // overall, might not be needed, when we are using JPMS
+    public interface Provider : Pluggable.Provider<LoggingBackend>
+//
+//    public companion object : PluggableBootstrap<LoggingBackend> by PluggableBootstrap.pluginLoader<LoggingBackend>() {
+//        public val Default: LoggingBackend get() = TODO()
+//        public fun bootstrap(value: LoggingBackend) {}
+//    }
+}
+
+// for companion object if single instance is used afterwards - can be abstract class to have logic - to api dependent
+public interface PluggableBootstrap<T : Any> {
+    public val Default: T
+    public fun bootstrap(value: T)
+}
+
+// automatically loaded
+@Plugin(LoggingBackend::class)
+public object LoggingBackendImpl : LoggingBackend
+
+// as in kx.serialization
+public fun LoggingBackend.Companion.pluginLoader(): PluginLoader<LoggingBackend> = TODO()
+
+public fun LoggingBackend.Companion.load(): Sequence<LoggingBackend> = TODO()
+
+// for JVM
+public fun LoggingBackend.Companion.load(classpath: String): Sequence<LoggingBackend> = TODO()
+
+// generated
+public interface Pluggable_LoggingBackend : Pluggable.Provider<LoggingBackend>
+
+/**
  * ```
  * // module: A
  * @JvmService // enables compatibility with java.util.ServiceLoader
  * @Pluggable
  * interface CryptographyProvider
  * @Pluggable interface LoggingBackend
+ *
+ * // generates if without
+ * interface Pluggable_LoggingBackend: Pluggable.Provider<LoggingBackend>
  *
  * // module: A, B or even in a library!
  * @Plugin(CryptographyProvider::class) object OpensslCryptographyProvider : CryptographyProvider
